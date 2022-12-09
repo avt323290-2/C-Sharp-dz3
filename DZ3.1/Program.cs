51253d069ecb5a8257b80553465b87be9c5c885c
Smirnov Mihail <118544468+avt323290-2@users.noreply.github.com>
2022-12-09 10:33:36 +0300

    Добавили код первой задачи

--- DZ3.1/Program.cs
+++ DZ3.1/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* 
+Задача 19 Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+14212 -> нет
+12821 -> да
+23432 -> да
+*/
+
+Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int len = number.Length;
+
+if (len == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine($"{number} - Ура! Это число палиндром!");
+    }
+    else
+    {
+        Console.WriteLine($"{number} - Увы! Это число не палиндром!");
+    }
+}
+else
+{
+    Console.WriteLine($"ОШИБКА: {number} - Это число не является пятизначным!");
+}
